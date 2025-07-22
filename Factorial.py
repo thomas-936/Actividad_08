4b45e1a671a1f7bb899f9c6c6bd5236b73f83e9b
tomas <tomas27@gmail.com>
2025-07-21 19:49:40 -0600

    primer commit

--- Factorial.py
+++ Factorial.py
@@ -1,1 +1,30 @@
 print("Actividad 08")
+opcion = 0
+
+def factorial (n)
+
+
+while (opcion!=6):
+    print("+++Menu+++")
+    print("1. Calcular el factorial de un número")
+    print("2. Suma de los primeros N números naturales")
+    print("3. Calcular el n-ésimo número de Fibonacci")
+    print("4. Contar cuántas veces aparece una letra en una palabra")
+    print("5.  Invertir una cadena de texto")
+    print("6. Calcular la potencia de un número (base^exponente)")
+    print("Salir del programa")
+
+    try:
+        opcion = int(input("Igrese su Opción: "))
+    except ValueError:
+        print("\nOpción Invalida... ")
+        continue
+
+    match opcion:
+
+
+
+
+
+
+
